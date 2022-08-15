34644e5fe484e8a0835c403efcfe7da4a14476a9
MacSadecki <mac.sadecki@gmail.com>
2022-08-15 16:13:11 +0200

    MadeBuildingsWork

--- Assets/Scripts/Menu/BuildingMenu.cs
+++ Assets/Scripts/Menu/BuildingMenu.cs
@@ -12,12 +12,15 @@ public class BuildingMenu : MonoBehaviour
     ResourceType Stone = (ResourceType)System.Enum.Parse(typeof(ResourceType), "Stone");
     ResourceType Food = (ResourceType)System.Enum.Parse(typeof(ResourceType), "Food");
     ResourceType Energy = (ResourceType)System.Enum.Parse(typeof(ResourceType), "Energy");
+        
 
     private bool enoughResources = false;
 
     private int woodCurrentAmount, stoneCurrentAmount, foodCurrentAmount, energyCurrentAmount;
 
-    public Image buildingImage; 
+    public Image buildingImage;
+
+    public GameObject upgradeButton;
 
     public TMP_Text titleText;
     public TMP_Text woodNeeded, StoneNeeded, EnergyNeeded, FoodNeeded;
@@ -25,17 +28,39 @@ public class BuildingMenu : MonoBehaviour
 
     private int buidlingLevel;
     private int buildingId = 0;
-   
 
-    int[,] ResourceNeeded = 
+
+    int[,] ResourceNeeded =
     {
         {10, 10, 10, 10},
+        {30, 30, 30, 30},
+        {50, 50, 50, 50},
+        {80, 80, 80, 80},
+        {120, 120, 120, 120},
+        {160, 160, 160, 160},
         {200, 200, 200, 200},
-        {300, 300, 300, 300},
-        {400, 400, 400, 400},
-        {500, 500, 500, 500}
+        {250, 250, 250, 250},
+        {300, 300, 300, 300}
 
     };
+
+    int[,] ResourceGiven =
+    {   
+        {0, 0, 0, 0}, //Level 0 Buildigs
+        {1, 1, 1, 1},
+        {2, 2, 2, 2},
+        {4, 4, 4, 4},
+        {6, 6, 6, 6},
+        {9, 9, 9, 9},
+        {12, 12, 12, 12},
+        {16, 16, 16, 16},
+        {20, 20, 20, 20},
+        {25, 25, 25, 25},
+        {30, 30, 30, 30}
+
+    };
+
+
 
     int[] BuildingLevels = 
     { 1, 1, 1, 1, 1, 1};
@@ -43,12 +68,12 @@ public class BuildingMenu : MonoBehaviour
 
     string[] Descriptions =
     {
-        "Pretty basic looking lumberyard",
-        "Quarry, it looks like it came from the STONE age. HA!",
-        "The one and only Granary",
-        "Solar Panels means free energy. Yay!",
-        "I can see the mountains from up here",
-        "Walls are a necessity here, without them I would be already gone."
+        "You know what a lumberyard is, right? Wood goes in, wood products go out. Just roll up your sleeves, or risk losing fingers. Do you even have sleeves?",
+        "This calls for a joke about stoning. Or getting stoned. You can’t be saying this with a stone cold face. Ha. Ha.",
+        "Food, batteries for human species. Don’t expect Michelin stars though. You’re no culinary critic either, mind you.",
+        "As much pain in the butt as it brought to fuel, coal and gas industries, solar energy has become a great, efficient source of renewable energy in the decades before the purge. Oh well, it’s not like you have any choice in the matter anymore, you filthy bio activist.",
+        "It has been said that certain scouts from certain fortresses can see their bases from them. It’s something that mercenary teams used to say around the 2000s, up until the Great Robot Crisis. There were no more scouts after that, unfortunately.",
+        "That’s the only thing stopping the enemies from getting to us, and destroying the infrastructure, since the cannons went silent. That’s it. No snarky remarks here. Can’t get too predictable now, can I?"
     };
 
     string[] BuildingNames =
@@ -114,10 +139,23 @@ public class BuildingMenu : MonoBehaviour
         titleText.text = BuildingNames[buildingId] + buidlingLevel;
         description.text = Descriptions[buildingId];
 
-        woodNeeded.text = ResourceNeeded[(buidlingLevel - 1), 0].ToString();
-        StoneNeeded.text = ResourceNeeded[(buidlingLevel - 1), 1].ToString();
-        EnergyNeeded.text = ResourceNeeded[(buidlingLevel - 1), 2].ToString();
-        FoodNeeded.text = ResourceNeeded[(buidlingLevel - 1), 3].ToString();
+        if (buidlingLevel < 10)
+        {
+            woodNeeded.text = ResourceNeeded[(buidlingLevel - 1), 0].ToString();
+            StoneNeeded.text = ResourceNeeded[(buidlingLevel - 1), 1].ToString();
+            EnergyNeeded.text = ResourceNeeded[(buidlingLevel - 1), 2].ToString();
+            FoodNeeded.text = ResourceNeeded[(buidlingLevel - 1), 3].ToString();
+            upgradeButton.SetActive(true);
+        }
+        else
+        {
+            woodNeeded.text = "-";
+            StoneNeeded.text = "-";
+            EnergyNeeded.text = "-";
+            FoodNeeded.text = "-";
+            upgradeButton.SetActive(false);
+
+        }
     }
 
 
@@ -234,7 +272,21 @@ public class BuildingMenu : MonoBehaviour
     private void Start()
     {
         UpdateUI();
+        InvokeRepeating("addingResources", 0, 1);
     }
+
+
+    private void addingResources()
+    {
+        ResourcesStorageController.GetComponent<ResourcesStorageController>().StoreResource(Wood,ResourceGiven[BuildingLevels[0], 0]);
+        ResourcesStorageController.GetComponent<ResourcesStorageController>().StoreResource(Stone, ResourceGiven[BuildingLevels[1], 1]);
+        ResourcesStorageController.GetComponent<ResourcesStorageController>().StoreResource(Food, ResourceGiven[BuildingLevels[2], 2]);
+        ResourcesStorageController.GetComponent<ResourcesStorageController>().StoreResource(Energy, ResourceGiven[BuildingLevels[3], 3]);
+
+    }
+
+
+
 
 
 }
